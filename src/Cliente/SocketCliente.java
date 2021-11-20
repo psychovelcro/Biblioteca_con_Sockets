72b323562f7aca84ea7eff0b92ffec9afdc5d26c
psychovelcro <juanreyfotos@gmail.com>
2021-11-20 18:10:32 +0100

    tab

--- src/Cliente/SocketCliente.java
+++ src/Cliente/SocketCliente.java
@@ -13,70 +13,47 @@ import java.util.Scanner;
 //cliente y mandarselos al sevidor para que nos devuelva una respuesta con 
 //el resultado de la suma de ambos.
 public class SocketCliente {
-	
-	//IP y Puerto a la que nos vamos a conectar
+
+	// IP y Puerto a la que nos vamos a conectar
 	public static final int PUERTO = 2017;
 	public static final String IP_SERVER = "localhost";
-	private String chorro,respuestaChorro;
-	
-	
-	
-	
+	private String chorro, respuestaChorro;
+
 	public SocketCliente(String chorro) {
 		this.chorro = chorro;
-		this.respuestaChorro="";
+		this.respuestaChorro = "";
 	}
-
-
-
 
 	public String enviarChorro() {
 		System.out.println("        APLICACI�N CLIENTE         ");
 		System.out.println("-----------------------------------");
-		
-		
+
 		InetSocketAddress direccionServidor = new InetSocketAddress(IP_SERVER, PUERTO);
-		
-		
-		try (Scanner sc = new Scanner(System.in);
-			Socket socketAlServidor = new Socket()){
-					
-		
+
+		try (Scanner sc = new Scanner(System.in); Socket socketAlServidor = new Socket()) {
+
 			System.out.println("CLIENTE: Introduzca los numeros a sumar");
-			String numero1 = sc.nextLine();//supongamos el numero 3
-			String numero2 = sc.nextLine();//supongamos el numero 4
-			
-			
-			String operandos = numero1 + "-" + numero2;//3-4
-			
-		
+			String numero1 = sc.nextLine();// supongamos el numero 3
+			String numero2 = sc.nextLine();// supongamos el numero 4
+
+			String operandos = numero1 + "-" + numero2;// 3-4
+
 			System.out.println("CLIENTE: Esperando a que el servidor acepte la conexi�n");
-			socketAlServidor.connect(direccionServidor);			
-			System.out.println("CLIENTE: Conexion establecida... a " + IP_SERVER 
-					+ " por el puerto " + PUERTO);	
-			
-			
-			
-			//Creamos el objeto que nos permite mandar informacion al servidor
+			socketAlServidor.connect(direccionServidor);
+			System.out.println("CLIENTE: Conexion establecida... a " + IP_SERVER + " por el puerto " + PUERTO);
+
+			// Creamos el objeto que nos permite mandar informacion al servidor
 			PrintStream salida = new PrintStream(socketAlServidor.getOutputStream());
-			
-			
-			
-			
-			salida.println(chorro);//3-4
-			
-			InputStreamReader entrada = new InputStreamReader(socketAlServidor.getInputStream());		
-			BufferedReader bf = new BufferedReader(entrada);	
+
+			salida.println(chorro);// 3-4
+
+			InputStreamReader entrada = new InputStreamReader(socketAlServidor.getInputStream());
+			BufferedReader bf = new BufferedReader(entrada);
 			System.out.println("CLIENTE: Esperando al resultado del servidor...");
-			
-			
-			
-			
-			respuestaChorro = bf.readLine();//7	
-			System.out.println("CLIENTE: El resultado de la suma es: " + respuestaChorro);//7
-			
-			
-			
+
+			respuestaChorro = bf.readLine();// 7
+			System.out.println("CLIENTE: El resultado de la suma es: " + respuestaChorro);// 7
+
 		} catch (UnknownHostException e) {
 			System.err.println("CLIENTE: No encuentro el servidor en la direcci�n" + IP_SERVER);
 			e.printStackTrace();
@@ -88,8 +65,7 @@ public class SocketCliente {
 			e.printStackTrace();
 		}
 		return respuestaChorro;
-		
-		
+
 	}
 
 }
