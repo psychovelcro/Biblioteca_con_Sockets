c7d959e96d78206c72adf19ef6305d3563966128
Adrian Cañadas <dragoadri@gmail.com>
2021-11-25 12:44:54 +0100

    Solo falta meter consultas

--- src/Cliente/ClienteHilo.java
+++ src/Cliente/ClienteHilo.java
@@ -1,5 +1,7 @@
 package Cliente;
 
+import java.util.ArrayList;
+
 public class ClienteHilo implements Runnable {
 private String consulta;
 private  SocketCliente sock;
@@ -10,14 +12,44 @@ private  SocketCliente sock;
 	this.sock = sock;
 }
 
-
-
 	@Override
 	public void run() {
 		
-		System.out.println(sock.enviarChorro(consulta));
+		
+		if(sock.enviarChorro(consulta).length()>5) {
+			System.out.println("Libros obtenidos de la consulta: \n");
+			for (Libro Libro : splitChorro(sock.enviarChorro(consulta))) {
+				
+				System.out.println(Libro.getIsbn()+" | "+Libro.getTitulo()+" | "+Libro.getAutor()+" | "
+						+Libro.getPrecio()+" euros.");
+				
+			}
+			
+		}
+		else {
+			System.out.println("Operacion hecha con exito ("+sock.enviarChorro(consulta)+")");
+		}
 		
 		
+		
+		
+		
+		
+	}
+	
+	
+	public static ArrayList<Libro> splitChorro(String chorro) {
+
+		ArrayList<Libro> libros = new ArrayList<Libro>();
+		String[] librosInfo = chorro.split("/");
+
+		for (String libro : librosInfo) {
+			String[] dataLibro = libro.split(",");
+			libros.add(new Libro(dataLibro[0], dataLibro[1], dataLibro[2], Double.parseDouble(dataLibro[3])));
+		}
+
+		return libros;
+
 	}
 
 }
--- src/Cliente/Main.java
+++ src/Cliente/Main.java
@@ -1,10 +1,14 @@
 package Cliente;
 
-public class Main {
+import java.util.ArrayList;
 
+public class Main {
+	private static ArrayList<String> consultasRandom;
+	
 	public static void main(String[] args) {
 		
-		
+		rellenarConsultas();
+	
 		SocketCliente sc= new SocketCliente();
 		
 		for (int i = 0; i < 10; i++) {
@@ -12,6 +16,27 @@ public class Main {
 			Thread c = new Thread(cHilo);
 			c.start();
 		}
+		
+	}
+
+	
+	private static int randomNumber() {
+		
+		
+		
+		return 0;
+	}
+
+	
+	private static void rellenarConsultas() {
+		consultasRandom = new ArrayList<>();
+		consultasRandom.add("");
+		consultasRandom.add("");
+		consultasRandom.add("");
+		consultasRandom.add("");
+		consultasRandom.add("");
+		consultasRandom.add("");
+
 		
 	}
 
